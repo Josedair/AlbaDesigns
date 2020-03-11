eff5cfd41118e630838e0bb4f8461a2d58a2790b
Jose Dair Guerrero Dominguez <005305010@coyote.csusb.edu>
2020-03-10 23:08:53 -0700

    e

---
+++ Controllers/Image_uploadController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class Image_uploadController : Controller
+    {
+        // GET: Image_upload
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public  ActionResult Add(Image_upload  imageModel)
+        {
+            string fileName = Path.GetFileNameWithoutExtension(imageModel.ImageFile.FileName);
+            string extension = Path.GetExtension(imageModel.ImageFile.FileName);
+            fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+            imageModel.ImagePath = "~/ImageUploads/" + fileName;
+            fileName = Path.Combine(Server.MapPath("~/ImageUploads"));
+            imageModel.ImageFile.SaveAs(fileName);
+            //using (DbModel db = new DbModel())
+            //{
+            //    db.Images.Add(imageModel);
+            //    db.SaveChanges();
+            //}
+            return View();
+        }
+    }
+}
